c42021e5daacbd18f88ba9a2aabd9087748a134d
adeliepen <seritaka@gmail.com>
2023-10-01 15:48:28 +0900

    update cheminfo_util

--- cheminfo_util.py
+++ cheminfo_util.py
@@ -2,15 +2,38 @@
 from rdkit import Chem
 from rdkit.Chem.MolStandardize import rdMolStandardize
 
-from chembl_structure_pipeline import standardizer
+from chembl_structure_pipeline import standardize_mol
 from chembl_structure_pipeline import get_parent_mol
 import chembl_structure_pipeline
 
 
 # 01 data_prep
+# Mainはコメントアウトしている方をActiveにする
+'''
 def prep_moleclue(mol):
+
+    raise NotImplementedError()
+'''
+
+def prep_moleclue(mol, canon_taut=True, get_large_frag=True):
     """
     @param mol: rdkit mol object
     @return tautomar canonicalized and standardized rdkit mol object
     """
-    raise NotImplementedError()
+    molprops = mol.GetPropsAsDict()
+    molname = mol.GetProp('_Name')
+    if canon_taut:
+        mol = rdMolStandardize.CanonicalTautomer(mol)
+    std_mol = standardize_mol(mol)
+    parent_mol, exclude= get_parent_mol(std_mol)
+    if exclude:
+        return None
+    if get_large_frag:
+        lfc = rdMolStandardize.LargestFragmentChooser()
+        uc = rdMolStandardize.Uncharger()
+        parent_mol = lfc.choose(parent_mol)
+        parent_mol = uc.uncharge(parent_mol)
+    for k,v in molprops.items():
+        parent_mol.SetProp(k, str(v))
+    parent_mol.SetProp('_Name', molname)
+    return parent_mol
